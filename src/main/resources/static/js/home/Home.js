974235ca055ea235fafe688678a56bae91a33eda
JeonYongJae <doldory33@gmail.com>
2023-01-03 06:58:33 +0900

    홈 작업

--- src/main/resources/static/js/home/Home.js
+++ src/main/resources/static/js/home/Home.js
@@ -4,8 +4,6 @@ const Home = {
             boardItems: [],
             topItems: [],
             middleItems: [],
-            middle1Items: [],
-            middle2Items: [],
             mainTitles: [],
         }
     },
@@ -31,8 +29,7 @@ const Home = {
                 this.topItems = this.boardItems.filter((elem => elem.attr1 === '홈상단'))
                 this.middleItems = this.boardItems.filter((elem => elem.attr1.indexOf('홈중간') > -1))
                 this.mainTitles = this.boardItems.filter((elem => elem.attr1.indexOf('홈타이틀') > -1))
-                // this.middle1Items = this.boardItems.filter((elem => elem.attr1 === '홈중간1'))
-                // this.middle2Items = this.boardItems.filter((elem => elem.attr1 === '홈중간2'))
+                
                 console.log(this.boardItems)
             }
           } catch (err) {
@@ -48,77 +45,45 @@ const Home = {
         },
     },
     template: `
-        <div class="d-flex justify-content-center pt-md-5">
-            <div id="carousel1" class="carousel slide" data-bs-ride="carousel" style="max-width:800px;">
+    
+        <div id="carousel1" class="carousel slide" data-bs-ride="carousel">
 
-                <p class="bg-title">{{ mainTitles[0].content }}</p>
-                
-                <div class="carousel-indicators">
-                    <button v-for="(item, idx) in topItems" :key="item.bno" type="button" data-bs-target="#carousel1" :data-bs-slide-to="idx" :class="{'active': idx === 0}" aria-current="true" :aria-label="'Slide' + (idx+1)"></button>
-                </div>
+            <p class="bg-title">{{ mainTitles[0].content }}</p>
 
-                <div class="carousel-inner">
-                    <div v-for="(item, idx) in topItems" :key="item.bno" :class="{'carousel-item': true, 'active': idx === 0}">
-                        <img :src="item.mnImg" class="d-block w-100" alt="">
-                        <div class="carousel-caption d-none d-md-block" v-html="item.content"></div>
-                    </div>
+            <div class="carousel-inner">
+                <div v-for="(item, idx) in topItems" :key="item.bno" :class="{'carousel-item': true, 'active': idx === 0}">
+                    <img :src="item.mnImg" class="d-block w-100" alt="">
+                    <div class="carousel-caption d-none d-md-block" v-html="item.content"></div>
                 </div>
-                
-                <!--<template v-if="topItems.length > 1">
-                <button class="carousel-control-prev" type="button" data-bs-target="#carousel1" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carousel1" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-                </template>-->
             </div>
+                            
         </div>
         
-        <div class="py-5">
-            <div class="container">
+        <div class="py-5 container-xxl t-pos">
 
-                <div v-if="middleItems.length > 0" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                    <div v-for="(item, idx) in middleItems" :key="item.bno" class="col">
-                        <div class="card shadow-sm">
+            <div v-if="middleItems.length > 0" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                <div v-for="(item, idx) in middleItems" :key="item.bno" class="col">
+                    <div class="card shadow-sm">
+                        <div class="card-header">
+                            Featured
+                        </div>
+                        <img :src="item.mnImg" class="bd-placeholder-img card-img-top d-block w-100" alt="">
 
-                            <img :src="item.mnImg" class="bd-placeholder-img card-img-top d-block w-100" alt="">
-
-                            <div class="card-body">
-                                <p class="card-text" v-html="item.content"></p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <template v-if="item.external_yn === 'N'">
-                                    <router-link :to="item.linkUrl" class="btn btn-primary">{{ item.subject }}</router-link>
-                                    </template>
-                                    <template v-else>
-                                    <a @click.prevent="click" :href="item.linkUrl" class="btn btn-primary">{{ item.subject }}</a>
-                                    </template>
-                                    <small class="text-muted">[{{ item.attr1 }}]</small>
-                                </div>
+                        <div class="card-body">
+                            <p class="card-text" v-html="item.content"></p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <template v-if="item.external_yn === 'N'">
+                                <router-link :to="item.linkUrl" class="btn btn-primary">{{ item.subject }}</router-link>
+                                </template>
+                                <template v-else>
+                                <a @click.prevent="click" :href="item.linkUrl" class="btn btn-primary">{{ item.subject }}</a>
+                                </template>
+                                <small class="text-muted">[{{ item.attr1 }}]</small>
                             </div>
                         </div>
                     </div>
                 </div>
-
-                <!--<div v-if="middle2Items.length > 0" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                    <div v-for="(item, idx) in middle2Items" :key="item.bno" class="col">
-                        <div class="card shadow-sm">
-
-                            <img :src="item.mnImg" class="bd-placeholder-img card-img-top d-block w-100" alt="">
-
-                            <div class="card-body">
-                                <p class="card-text" v-html="item.content"></p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <a href="#" class="btn btn-primary">이동</a>
-                                    <small class="text-muted">[{{ item.attr1 }}]</small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>-->
-
             </div>
-        </div>     
+
+        </div>
 `}

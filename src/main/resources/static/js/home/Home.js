6fe276b62b6cda39f99b98922747b45705dd0723
unknown <doldory33@gmail.com>
2023-02-04 17:17:42 +0900

    메인페이지 수정

--- src/main/resources/static/js/home/Home.js
+++ src/main/resources/static/js/home/Home.js
@@ -24,15 +24,15 @@ const Home = {
             })
             console.log(result)
             if (result.data && result.data.result == 'success') {
-              this.boardItems = result.data.data
+                this.boardItems = result.data.data
                 this.boardItems = this.boardItems.map((elem, idx, arr) => {
                     elem.mnImg = elem.attchFiles.length > 0 ? this.$comm.imgURL + elem.attchFiles[0].file_nm : this.$comm.noImgURL
                     elem.linkUrl = elem.link_url || ''
                     return elem
                 })
-                this.topItems = this.boardItems.filter((elem => elem.attr1 === '홈상단'))
-                this.middleItems = this.boardItems.filter((elem => elem.attr1.indexOf('홈중간') > -1))
-                this.mainTitles = this.boardItems.filter((elem => elem.attr1.indexOf('홈타이틀') > -1))
+                this.topItems = this.boardItems.filter((elem => elem.attr1 === '상단'))
+                this.middleItems = this.boardItems.filter((elem => elem.attr1.indexOf('중간') > -1))
+                this.mainTitles = this.boardItems.filter((elem => elem.attr1.indexOf('타이틀') > -1))
                 this.title = this.mainTitles[0].content
                 
                 console.log(this.boardItems)
@@ -65,27 +65,33 @@ const Home = {
         
         <div class="py-5 container-xxl t-pos">
 
-            <div v-if="middleItems.length > 0" class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xxl-4 g-3">
+            <div v-if="middleItems.length > 0" class="row g-3 row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xxl-4">
+
                 <div v-for="(item, idx) in middleItems" :key="item.bno" class="col">
+
                     <div class="card shadow-sm">
-                        <div class="card-header">
-                            Featured
-                        </div>
-                        <img :src="item.mnImg" class="bd-placeholder-img card-img-top d-block w-100" alt="">
+                        
+                        <div class="card-header fs-5 fw-bold">{{ item.subject }}</div>
+
+                        <a :href="item.linkUrl">
+                            <img :src="item.mnImg" class="bd-placeholder-img card-img-top d-block w-100" alt="">
+                        </a>
 
                         <div class="card-body">
                             <p class="card-text" v-html="item.content"></p>
-                            <div class="d-flex justify-content-between align-items-center">
+                            <!--<div class="d-flex justify-content-between align-items-center">
                                 <template v-if="item.external_yn === 'N'">
-                                <router-link :to="item.linkUrl" class="btn btn-primary">{{ item.subject }}</router-link>
+                                <router-link :to="item.linkUrl" class="btn btn-primary">바로가기</router-link>
                                 </template>
                                 <template v-else>
-                                <a @click.prevent="click" :href="item.linkUrl" class="btn btn-primary">{{ item.subject }}</a>
+                                <a @click.prevent="click" :href="item.linkUrl" class="btn btn-primary">바로가기</a>
                                 </template>
                                 <small class="text-muted">[{{ item.attr1 }}]</small>
-                            </div>
+                            </div>-->
                         </div>
+
                     </div>
+
                 </div>
             </div>
 
